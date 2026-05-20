6cea478d2c7333cd50aaaf3acb1648a4f3dcb1a8
tonyopuodho <tonyopuodho@gmail.com>
2026-05-20 16:31:24 +0300

    Implementing different routes and navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import { Fragment } from "react"
+import NavigationBar from "./components/NavigationBar"
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Favorites from "./pages/Favorites"
+import RecipeDetails from "./pages/RecipeDetails"
 
 
 function App() {
   
   return (
-   <Fragment></Fragment>
+   <Fragment>
+    <NavigationBar/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/favorites" element={<Favorites/>}/>
+      <Route path="/recipeDetails/:id" element={<RecipeDetails/>}/>
+    </Routes>
+   </Fragment>
   )
 }
 
---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function NavigationBar() {
+  return (
+    <div className='bg-amber-900 w-full h-[10vh] font-serif'>
+      <div className='max-w-7xl md:w-[80%] mx-auto px-2 items-center'>
+        <h1 className='text-white text-4xl font-extrabold'>FoodRecipe</h1>
+      </div>
+    </div>
+  )
+}
+
+export default NavigationBar
