7894d60274a8971416269c42506507e70ff1b59f
tonyopuodho <tonyopuodho@gmail.com>
2026-05-20 19:59:22 +0300

    Implementing product card that displays product details

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -7,19 +7,16 @@ export default function GlobalState({children}){
     const [products,setProducts] = useState([])
     async function handleSubmit(event) {
         event.preventDefault()
-       try {
-        
+       try {        
             const apiRequest = await fetch(`https://forkify-api.jonas.io/api/v2/recipes?search=${searchParams}&key=76bf349e-c0b1-49c6-b375-686b22c6dfe1`)
             const apiResponse = await apiRequest.json()
             
             if (apiResponse?.data.recipes){
                 setProducts(apiResponse?.data.recipes)
             }
-
-            console.log(products)
        } catch (error) {
          console.log(error)
        }
     }
-    return <GlobalContext.Provider value={{searchParams,setSearchParams,handleSubmit}}>{children}</GlobalContext.Provider>
+    return <GlobalContext.Provider value={{searchParams,setSearchParams,handleSubmit,products}}>{children}</GlobalContext.Provider>
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React, { useContext } from 'react'
 import { GlobalContext } from '../context/context'
+import ProductCard from '../components/ProductCard'
 
 function Home() {
-  const { searchParams,setSearchParams,handleSubmit } = useContext(GlobalContext)
-  console.log(searchParams)
+  const { searchParams,setSearchParams,handleSubmit,products } = useContext(GlobalContext)
+  console.log(products)
   return (
-    <div className='bg-gray-100 font-serif w-full h-[100vh] py-10'>
+    <div className='bg-gray-100 font-serif w-full h-full py-10'>
       <div className='max-w-7xl md:w-[80%] mx-auto px-2 w-full'>
         <form onSubmit={handleSubmit} className='flex md:flex-row  w-full gap-2 flex-col justify-center items-center'>
           <input type="text"
@@ -14,6 +15,15 @@ function Home() {
           placeholder='Enter food...' className=' bg-white p-2 md:w-[50%] w-full rounded-md shadow-sm'/>
           <button className='py-2 bg-amber-900 rounded-md cursor-pointer text-white font-bold w-full md:w-[20%]'>Search recipe</button>
         </form>
+        <div className='mt-10'>
+          {
+            products.length > 0 ? <div className='grid grid-cols-2 md:grid-cols-4 gap-2'>
+              {
+                products.map((item) => <ProductCard item={item}/> )
+              }
+            </div> : <h1 className='font-bold text-center text-2xl'>No recipes found enter your food to get a recipe</h1>
+          }
+        </div>
       </div>
     </div>
   )
